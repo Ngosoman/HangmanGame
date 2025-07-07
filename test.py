77520d52571917e75f2e7b1909ba5df3dba9378c
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-07 09:48:57 +0300

    Add display_word function to show current guessed letters in the game

--- test.py
+++ test.py
@@ -7,3 +7,7 @@ word = random.choice(words)
 # Game state
 guessed = []
 tries = 6
+
+# Display word with underscores
+def display_word():
+    return " ".join([letter if letter in guessed else "_" for letter in word])
